d2ebd210265001e625f4cc3620ac43d8b1373cf4
sheng <86102390+sheng120777@users.noreply.github.com>
2021-09-11 22:00:00 +0800

    Update pC.cpp

--- NHDK/round7/div2/pC.cpp
+++ NHDK/round7/div2/pC.cpp
@@ -2,25 +2,21 @@
 using namespace std;
 #define int long long
 
-vector<int>vec;
-
 signed main(){
 
     ios::sync_with_stdio(0);
     cin.tie(0);
     
     int n,k;
+    int ans=0;
     cin>>n>>k;
     while(n--){
-        int t;
-        cin>>t;
-        vec.push_back(t);
-    }
-    int ans;
-    for (int i=0;i<vec.size();i++){
-        if (vec[i]>k){
+        int input;
+        cin>>input;
+        if(input>k){
             ans++;
         }
     }
+
     cout<<ans;
 }
