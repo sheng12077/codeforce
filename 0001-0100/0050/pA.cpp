d7705fe241661f94cdcb03e35d7e23a9599bef7d
sheng <86102390+sheng120777@users.noreply.github.com>
2021-09-29 13:54:02 +0800

    Create pA.cpp

---
+++ 0001-0100/0050/pA.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl "\n"
+
+signed  main(){
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n,m;
+    cin>>n>>m;
+    cout<<(n*m)/2<<endl;
+}
