8bf6dd291bdbc992987155a39aba40ee0b287ed9
sheng12077 <141042325+sheng12077@users.noreply.github.com>
2023-12-18 00:33:07 +0800

    Create pA.cpp

---
+++ NHDK/round36/pA.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl '\n'
+#define inf 2e18
+#define maxn 100005
+#define mod 1000000007
+#define mem(x) memset(x,0,sizeof(x))
+
+signed main(){
+
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+
+    int n;
+    cin>>n;
+    cout<<"autoputto "<<n<<" no mahoo";
+}
