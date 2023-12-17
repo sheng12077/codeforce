792d6f8744ce8cb72bbdd51b63029241d6be5fba
sheng12077 <141042325+sheng12077@users.noreply.github.com>
2023-12-18 00:38:19 +0800

    Create pB.cpp

---
+++ NHDK/round36/pB.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl '\n'
+#define inf 2e18
+#define maxn 100005
+#define mod 1000000007
+#define mem(x) memset(x,0,sizeof(x))
+
+signed main(){
+
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+
+    int x,y;
+    cin>>x>>y;
+    cout<<(1+y)*y/2-(x-1)*x/2;
+}
