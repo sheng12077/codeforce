55f270b11a19c374d15dd70704f86c57c1b5f7fd
sheng12077 <141042325+sheng12077@users.noreply.github.com>
2023-12-18 00:40:23 +0800

    Create pC.cpp

---
+++ NHDK/round36/pC.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl '\n'
+#define inf 2e18
+#define maxn 100005
+#define mod 1000000007
+#define mem(x) memset(x,0,sizeof(x))
+
+signed main(){
+
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+
+    int a,b,c,d;
+    cin>>a>>b>>c>>d;
+    if(a==c && b==d){
+        cout<<"Yes";
+    }
+    else{
+        cout<<"No";
+    }
+}
